27c6f6c1bd1082d8146d722ad5442ea2898b5bde
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-11-27 17:04:31 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let arr = array.find( function(s) { return s.result === "W" })
+    if(arr)
+    return arr.year ;
+}
